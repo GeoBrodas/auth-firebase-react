c58538cf804627dad36e50c662b664199df2c633
Georgey <geobro2310@gmail.com>
2021-03-13 19:45:08 +0530

    Add dashboard, signup, login

--- src/components/Login.js
+++ src/components/Login.js
@@ -6,8 +6,8 @@ import { useAuth } from "../context/AuthContext";
 function Signup() {
   const emailRef = useRef();
   const passwordRef = useRef();
-  const passwordConfirmRef = useRef();
-  const { signup } = useAuth();
+
+  const { login } = useAuth();
   const [error, setError] = useState("");
   const [loading, setLoading] = useState(false);
   const history = useHistory();
@@ -15,17 +15,13 @@ function Signup() {
   async function handleSubmit(e) {
     e.preventDefault();
 
-    if (passwordRef.current.value !== passwordConfirmRef.current.value) {
-      return setError("Passwords do not match");
-    }
-
     try {
       setError("");
       setLoading(true);
-      await signup(emailRef.current.value, passwordRef.current.value);
+      await login(emailRef.current.value, passwordRef.current.value);
       history.push("/");
     } catch {
-      setError("Oops try again, try entering a 6 letter password");
+      setError("Oops try again, maybe you don't have an account🤷‍♂️");
     }
 
     setLoading(false);
@@ -35,7 +31,7 @@ function Signup() {
     <>
       <Card>
         <Card.Body>
-          <h2 className="text-center mb-4">Sign Up</h2>
+          <h2 className="text-center mb-4">Log In</h2>
           {error && <Alert variant="danger">{error}</Alert>}
           <Form onSubmit={handleSubmit}>
             <Form.Group id="email">
@@ -46,18 +42,15 @@ function Signup() {
               <Form.Label>Password</Form.Label>
               <Form.Control type="password" ref={passwordRef} required />
             </Form.Group>
-            <Form.Group id="password-confirm">
-              <Form.Label>Confirm Password</Form.Label>
-              <Form.Control type="password" ref={passwordConfirmRef} required />
-            </Form.Group>
+
             <Button disabled={loading} className="w-100" type="submit">
-              Sign Up!!
+              Log In
             </Button>
           </Form>
         </Card.Body>
       </Card>
       <div className="w-100 text-center mt-2">
-        Already have an account? <Link to="/login">Log In</Link>
+        No Account? <Link to="/signup">Sign Up</Link>
       </div>
     </>
   );
